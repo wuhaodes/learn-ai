95f8e0f2c7dac2ab9b890c958e4b2cdf9bf0e1db
wuhaodes <wuhaodes@qq.com>
2025-07-25 09:11:35 +0800

    每天早晨的自学--周五

--- prompt.py
+++ prompt.py
@@ -761,3 +761,210 @@ from tool import get_completion
 #     """
 #     print(f"评论{i+1}:\n", get_completion(prompt), "\n")
 
+# prod_review = """
+# Got this panda plush toy for my daughter's birthday, \
+# who loves it and takes it everywhere. It's soft and \
+# super cute, and its face has a friendly look. It's \
+# a bit small for what I paid though. I think there \
+# might be other options that are bigger for the \
+# same price. It arrived a day earlier than expected, \
+# so I got to play with it myself before I gave it \
+# to her.
+# """
+# prompt = f"""
+# Your task is to generate a short summary of a product \
+# review from an ecommerce site.
+# Summarize the review below, delimited by triple
+# backticks, in at most 30 words.
+# Review: ```{prod_review}```
+# """
+# response = get_completion(prompt)
+# print(response)
+
+# prompt = f"""
+# Your task is to generate a short summary of a product \
+# review from an ecommerce site to give feedback to the \
+# Shipping deparmtment.
+# Summarize the review below, delimited by triple
+# backticks, in at most 30 words, and focusing on any aspects \
+# that mention shipping and delivery of the product.
+# Review: ```{prod_review}```
+# """
+# response = get_completion(prompt)
+# print(response)
+
+# prompt = f"""
+# Your task is to generate a short summary of a product \
+# review from an ecommerce site to give feedback to the \
+# pricing deparmtment, responsible for determining the \
+# price of the product.
+# Summarize the review below, delimited by triple
+# backticks, in at most 30 words, and focusing on any aspects \
+# that are relevant to the price and perceived value.
+# Review: ```{prod_review}```
+# """
+# response = get_completion(prompt)
+# print(response)
+
+# prompt = f"""
+# Your task is to extract relevant information from \
+# a product review from an ecommerce site to give \
+# feedback to the Shipping department.
+# From the review below, delimited by triple quotes \
+# extract the information relevant to shipping and \
+# delivery. Limit to 30 words.
+# Review: ```{prod_review}```
+# """
+# response = get_completion(prompt)
+# print(response)
+
+# review_1 = prod_review
+# # review for a standing lamp
+# review_2 = """
+# Needed a nice lamp for my bedroom, and this one \
+# had additional storage and not too high of a price \
+# point. Got it fast - arrived in 2 days. The string \
+# to the lamp broke during the transit and the company \
+# happily sent over a new one. Came within a few days \
+# as well. It was easy to put together. Then I had a \
+# missing part, so I contacted their support and they \
+# very quickly got me the missing piece! Seems to me \
+# to be a great company that cares about their customers \
+# and products.
+# """
+# # review for an electric toothbrush
+# review_3 = """
+# My dental hygienist recommended an electric toothbrush, \
+# which is why I got this. The battery life seems to be \
+# pretty impressive so far. After initial charging and \
+# leaving the charger plugged in for the first week to \
+# condition the battery, I've unplugged the charger and \
+# been using it for twice daily brushing for the last \
+# 3 weeks all on the same charge. But the toothbrush head \
+# is too small. I’ve seen baby toothbrushes bigger than \
+# this one. I wish the head was bigger with different \
+# length bristles to get between teeth better because \
+# this one doesn’t. Overall if you can get this one \
+# around the $50 mark, it's a good deal. The manufactuer's \
+# replacements heads are pretty expensive, but you can \
+# get generic ones that're more reasonably priced. This \
+# toothbrush makes me feel like I've been to the dentist \
+# every day. My teeth feel sparkly clean!
+# """
+# # review for a blender
+# review_4 = """
+# So, they still had the 17 piece system on seasonal \
+# sale for around $49 in the month of November, about \
+# half off, but for some reason (call it price gouging) \
+# around the second week of December the prices all went \
+# up to about anywhere from between $70-$89 for the same \
+# system. And the 11 piece system went up around $10 or \
+# so in price also from the earlier sale price of $29. \
+# So it looks okay, but if you look at the base, the part \
+# where the blade locks into place doesn’t look as good \
+# as in previous editions from a few years ago, but I \
+# plan to be very gentle with it (example, I crush \
+# very hard items like beans, ice, rice, etc. in the \
+# blender first then pulverize them in the serving size \
+# I want in the blender then switch to the whipping \
+# blade for a finer flour, and use the cross cutting blade \
+# first when making smoothies, then use the flat blade \
+# if I need them finer/less pulpy). Special tip when making \
+# smoothies, finely cut and freeze the fruits and \
+# vegetables (if using spinach-lightly stew soften the \
+# spinach then freeze until ready for use-and if making \
+# sorbet, use a small to medium sized food processor) \
+# that you plan to use that way you can avoid adding so \
+# much ice if at all-when making your smoothie. \
+# After about a year, the motor was making a funny noise. \
+# I called customer service but the warranty expired \
+# already, so I had to buy another one. FYI: The overall \
+# quality has gone done in these types of products, so \
+# they are kind of counting on brand recognition and \
+# consumer loyalty to maintain sales. Got it in about \
+# two days
+# """
+
+# reviews = [review_1, review_2, review_3, review_4]
+
+# for i in range(len(reviews)):
+#     prompt = f"""
+#     Your task is to generate a short summary of a product \
+#     review from an ecommerce site.
+#     Summarize the review below, delimited by triple \
+#     backticks in at most 20 words.
+#     Review: ```{reviews[i]}```
+#     """
+#     response = get_completion(prompt)   
+#     print(i+1, response, "\n")
+
+# lamp_review = """
+# 我需要一盏漂亮的卧室灯，这款灯具有额外的储物功能，价格也不算太高。\
+# 我很快就收到了它。在运输过程中，我们的灯绳断了，但是公司很乐意寄送了一个新的。\
+# 几天后就收到了。这款灯很容易组装。我发现少了一个零件，于是联系了他们的客服，他们很快就给我寄来了缺
+# 失的零件！\
+# 在我看来，Lumina 是一家非常关心顾客和产品的优秀公司！
+# """
+
+# prompt = f"""
+# 以下用三个反引号分隔的产品评论的情感是什么？
+# 评论文本: ```{lamp_review}```
+# """
+# response = get_completion(prompt)
+# print(response)
+
+# prompt = f"""
+# 以下用三个反引号分隔的产品评论的情感是什么？
+# 用一个单词回答：「正面」或「负面」。
+# 评论文本: ```{lamp_review}```
+# """
+# response = get_completion(prompt)
+# print(response)
+
+# 中文
+# prompt = f"""
+# 识别以下评论的作者表达的情感。包含不超过五个项目。将答案格式化为以逗号分隔的中文单词列表。
+# 评论文本: ```{lamp_review}```
+# """
+# response = get_completion(prompt)
+# print(response)
+
+# # 中文
+# prompt = f"""
+# 以下评论的作者是否表达了愤怒？评论用三个反引号分隔。给出是或否的答案。
+# 评论文本: ```{lamp_review}```
+# """
+# response = get_completion(prompt)
+# print(response)
+
+# # 中文
+# prompt = f"""
+# 从评论文本中识别以下项目：
+# - 评论者购买的物品
+# - 制造该物品的公司
+# 评论文本用三个反引号分隔。将你的响应格式化为以 “物品” 和 “品牌” 为键的 JSON 对象。
+# 如果信息不存在，请使用 “未知” 作为值。
+# 让你的回应尽可能简短。
+# 评论文本: ```{lamp_review}```
+# """
+# response = get_completion(prompt)
+# print(response)
+
+# # 中文
+# prompt = f"""
+# 从评论文本中识别以下项目：
+# - 情绪（正面或负面）
+# - 审稿人是否表达了愤怒？（是或否）
+# - 评论者购买的物品
+# - 制造该物品的公司
+# 评论用三个反引号分隔。将你的响应格式化为 JSON 对象，以 “情感倾向”、“是否生气”、“物品类型” 和
+# “品牌” 作为键。
+# 如果信息不存在，请使用 “未知” 作为值。
+# 让你的回应尽可能简短。
+# 将 “是否生气” 值格式化为布尔值。
+# 评论文本: ```{lamp_review}```
+# """
+# response = get_completion(prompt)
+# print(response)
+
+# 学习到 <<三、主题推断>>
