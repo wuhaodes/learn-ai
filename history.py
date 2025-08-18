c033becc7d043356f3bfd6d4f3aefe0c7ea08a61
wuhaodes <wuhaodes@qq.com>
2025-08-18 11:30:12 +0800

    ai自学--周一

--- history.py
+++ history.py
@@ -2230,3 +2230,551 @@
 # )
 
 # dashboard
+
+# 第九章 评估（上）——存在一个简单的正确答案
+# import utils_zh
+# import json
+# import time
+# from tool import get_completion_from_messages
+
+# products_and_category = utils_zh.get_products_and_category()
+# print(products_and_category)
+
+
+# def find_category_and_product_v1(user_input, products_and_category):
+#     """
+#     从用户输入中获取到产品和类别
+#     参数：
+#     user_input：用户的查询
+#     products_and_category：产品类型和对应产品的字典
+#     """
+#     delimiter = "####"
+#     system_message = f"""
+#     您将提供客户服务查询。\
+#     客户服务查询将用{delimiter}字符分隔。
+#     输出一个 Python 列表，列表中的每个对象都是 Json 对象，每个对象的格式如下：
+#     '类别': <电脑和笔记本, 智能手机和配件, 电视和家庭影院系统, \
+#     游戏机和配件, 音频设备, 相机和摄像机中的一个>,
+#     以及
+#     '名称': <必须在下面允许的产品中找到的产品列表>
+#     其中类别和产品必须在客户服务查询中找到。
+#     如果提到了一个产品，它必须与下面允许的产品列表中的正确类别关联。
+#     如果没有找到产品或类别，输出一个空列表。
+#     根据产品名称和产品类别与客户服务查询的相关性，列出所有相关的产品。
+#     不要从产品的名称中假设任何特性或属性，如相对质量或价格。
+#     允许的产品以 JSON 格式提供。
+#     每个项目的键代表类别。
+#     每个项目的值是该类别中的产品列表。
+#     允许的产品：{products_and_category}
+#     """
+#     few_shot_user_1 = """我想要最贵的电脑。"""
+#     few_shot_assistant_1 = """
+#     [{'category': '电脑和笔记本', \
+#     'products': ['TechPro 超极本', 'BlueWave 游戏本', 'PowerLite Convertible', 'TechPro
+#     Desktop', 'BlueWave Chromebook']}]
+#     """
+#     messages = [
+#         {"role": "system", "content": system_message},
+#         {"role": "user", "content": f"{delimiter}{few_shot_user_1}{delimiter}"},
+#         {"role": "assistant", "content": few_shot_assistant_1},
+#         {"role": "user", "content": f"{delimiter}{user_input}{delimiter}"},
+#     ]
+#     return get_completion_from_messages(messages)
+
+
+# customer_msg_0 = f"""如果我预算有限，我可以买哪款电视？"""
+# products_by_category_0 = find_category_and_product_v1(
+#     customer_msg_0, products_and_category
+# )
+# print(products_by_category_0)
+
+# customer_msg_1 = f"""我需要一个智能手机的充电器"""
+# products_by_category_1 = find_category_and_product_v1(
+#     customer_msg_1, products_and_category
+# )
+# print(products_by_category_1)
+
+# customer_msg_2 = f"""
+# 你们有哪些电脑？"""
+# products_by_category_2 = find_category_and_product_v1(
+#     customer_msg_2, products_and_category
+# )
+# print(products_by_category_2)
+
+
+# customer_msg_3 = f"""
+# 告诉我关于smartx pro手机和fotosnap相机的信息，那款DSLR的。
+# 我预算有限，你们有哪些性价比高的电视推荐？"""
+# products_by_category_3 = find_category_and_product_v1(
+#     customer_msg_3, products_and_category
+# )
+# print(products_by_category_3)
+
+# customer_msg_4 = f"""
+# 告诉我关于CineView电视的信息，那款8K的，还有Gamesphere游戏机，X款的。
+# 我预算有限，你们有哪些电脑？"""
+# products_by_category_4 = find_category_and_product_v1(customer_msg_4,products_and_category)
+# print(products_by_category_4)
+
+
+# def find_category_and_product_v2(user_input, products_and_category):
+#     """
+#     从用户输入中获取到产品和类别
+#     添加：不要输出任何不符合 JSON 格式的额外文本。
+#     添加了第二个示例（用于 few-shot 提示），用户询问最便宜的计算机。
+#     在这两个 few-shot 示例中，显示的响应只是 JSON 格式的完整产品列表。
+#     参数：
+#     user_input：用户的查询
+#     products_and_category：产品类型和对应产品的字典
+#     """
+#     delimiter = "####"
+#     system_message = f"""
+#     您将提供客户服务查询。\
+#     客户服务查询将用{delimiter}字符分隔。
+#     输出一个 Python列表，列表中的每个对象都是 JSON 对象，每个对象的格式如下：
+#     '类别': <电脑和笔记本, 智能手机和配件, 电视和家庭影院系统, \
+#     游戏机和配件, 音频设备, 相机和摄像机中的一个>,
+#     以及
+#     '名称': <必须在下面允许的产品中找到的产品列表>
+#     不要输出任何不是 JSON 格式的额外文本。
+#     输出请求的 JSON 后，不要写任何解释性的文本。
+#     其中类别和产品必须在客户服务查询中找到。
+#     如果提到了一个产品，它必须与下面允许的产品列表中的正确类别关联。
+#     如果没有找到产品或类别，输出一个空列表。
+#     根据产品名称和产品类别与客户服务查询的相关性，列出所有相关的产品。
+#     不要从产品的名称中假设任何特性或属性，如相对质量或价格。
+#     允许的产品以 JSON 格式提供。
+#     每个项目的键代表类别。
+#     每个项目的值是该类别中的产品列表。
+#     允许的产品：{products_and_category}
+#     """
+#     few_shot_user_1 = """我想要最贵的电脑。你推荐哪款？"""
+#     few_shot_assistant_1 = """
+#     [{'category': '电脑和笔记本', \
+#     'products': ['TechPro 超极本', 'BlueWave 游戏本', 'PowerLite Convertible', 'TechPro
+#     Desktop', 'BlueWave Chromebook']}]
+#     """
+#     few_shot_user_2 = """我想要最便宜的电脑。你推荐哪款？"""
+#     few_shot_assistant_2 = """
+#     [{'category': '电脑和笔记本', \
+#     'products': ['TechPro 超极本', 'BlueWave 游戏本', 'PowerLite Convertible', 'TechPro
+#     Desktop', 'BlueWave Chromebook']}]
+#     """
+#     messages = [
+#         {"role": "system", "content": system_message},
+#         {"role": "user", "content": f"{delimiter}{few_shot_user_1}{delimiter}"},
+#         {"role": "assistant", "content": few_shot_assistant_1},
+#         {"role": "user", "content": f"{delimiter}{few_shot_user_2}{delimiter}"},
+#         {"role": "assistant", "content": few_shot_assistant_2},
+#         {"role": "user", "content": f"{delimiter}{user_input}{delimiter}"},
+#     ]
+#     return get_completion_from_messages(messages)
+
+
+# # customer_msg_3 = f"""
+# # 告诉我关于smartx pro手机和fotosnap相机的信息，那款DSLR的。
+# # 另外，你们有哪些电视？"""
+# # products_by_category_3 = find_category_and_product_v2(
+# #     customer_msg_3, products_and_category
+# # )
+# # print(products_by_category_3)
+
+# # customer_msg_0 = f"""如果我预算有限，我可以买哪款电视？"""
+# # products_by_category_0 = find_category_and_product_v2(
+# #     customer_msg_0, products_and_category
+# # )
+# # print(products_by_category_0)
+
+
+# msg_ideal_pairs_set = [
+#     # eg 0
+#     {
+#         "customer_msg": """如果我预算有限，我可以买哪种电视？""",
+#         "ideal_answer": {
+#             "电视和家庭影院系统": set(
+#                 [
+#                     "CineView 4K TV",
+#                     "SoundMax Home Theater",
+#                     "CineView 8K TV",
+#                     "SoundMax Soundbar",
+#                     "CineView OLED TV",
+#                 ]
+#             )
+#         },
+#     },
+#     # eg 1
+#     {
+#         "customer_msg": """我需要一个智能手机的充电器""",
+#         "ideal_answer": {
+#             "智能手机和配件": set(
+#                 ["MobiTech PowerCase", "MobiTech Wireless Charger", "SmartX EarBuds"]
+#             )
+#         },
+#     },
+#     # eg 2
+#     {
+#         "customer_msg": f"""你有什么样的电脑""",
+#         "ideal_answer": {
+#             "电脑和笔记本": set(
+#                 [
+#                     "TechPro 超极本",
+#                     "BlueWave 游戏本",
+#                     "PowerLite Convertible",
+#                     "TechPro Desktop",
+#                     "BlueWave Chromebook",
+#                 ]
+#             )
+#         },
+#     },
+#     # eg 3
+#     # {
+#     #     "customer_msg": f"""告诉我关于smartx pro手机和fotosnap相机的信息，那款DSLR的。另外，你们有哪些电视？""",
+#     #     "ideal_answer": {
+#     #         "智能手机和配件": set(["SmartX ProPhone"]),
+#     #         "相机和摄像机": set(["FotoSnap DSLR Camera"]),
+#     #         "电视和家庭影院系统": set(
+#     #             [
+#     #                 "CineView 4K TV",
+#     #                 "SoundMax Home Theater",
+#     #                 "CineView 8K TV",
+#     #                 "SoundMax Soundbar",
+#     #                 "CineView OLED TV",
+#     #             ]
+#     #         ),
+#     #     },
+#     # },
+#     # # eg 4
+#     # {
+#     #     "customer_msg": """告诉我关于CineView电视，那款8K电视、Gamesphere游戏机和X游戏机的信息。我的预算有限，你们有哪些电脑？""",
+#     #     "ideal_answer": {
+#     #         "电视和家庭影院系统": set(["CineView 8K TV"]),
+#     #         "游戏机和配件": set(["GameSphere X"]),
+#     #         "电脑和笔记本": set(
+#     #             [
+#     #                 "TechPro Ultrabook",
+#     #                 "BlueWave Gaming Laptop",
+#     #                 "PowerLiteConvertible",
+#     #                 "TechPro Desktop",
+#     #                 "BlueWave Chromebook",
+#     #             ]
+#     #         ),
+#     #     },
+#     # },
+#     # eg 5
+#     {
+#         "customer_msg": f"""你们有哪些智能手机""",
+#         "ideal_answer": {
+#             "智能手机和配件": set(
+#                 [
+#                     "SmartX ProPhone",
+#                     "MobiTech PowerCase",
+#                     "SmartX MiniPhone",
+#                     "MobiTech Wireless Charger",
+#                     "SmartX EarBuds",
+#                 ]
+#             )
+#         },
+#     },
+#     # eg 6
+#     {
+#         "customer_msg": f"""我预算有限。你能向我推荐一些智能手机吗？""",
+#         "ideal_answer": {
+#             "智能手机和配件": set(
+#                 [
+#                     "SmartX EarBuds",
+#                     "SmartX MiniPhone",
+#                     "MobiTech PowerCase",
+#                     "SmartXProPhone",
+#                     "MobiTech Wireless Charger",
+#                 ]
+#             )
+#         },
+#     },
+#     # eg 7 # this will output a subset of the ideal answer
+#     {
+#         "customer_msg": f"""有哪些游戏机适合我喜欢赛车游戏的朋友？""",
+#         "ideal_answer": {
+#             "游戏机和配件": set(
+#                 [
+#                     "GameSphere X",
+#                     "ProGamer Controller",
+#                     "GameSphere Y",
+#                     "ProGamer Racing Wheel",
+#                     "GameSphere VR Headset",
+#                 ]
+#             )
+#         },
+#     },
+#     # eg 8
+#     {
+#         "customer_msg": f"""送给我摄像师朋友什么礼物合适？""",
+#         "ideal_answer": {
+#             "相机和摄像机": set(
+#                 [
+#                     "FotoSnap DSLR Camera",
+#                     "ActionCam 4K",
+#                     "FotoSnap Mirrorless Camera",
+#                     "ZoomMaster Camcorder",
+#                     "FotoSnap Instant Camera",
+#                 ]
+#             )
+#         },
+#     },
+#     # eg 9
+#     {"customer_msg": f"""我想要一台热水浴缸时光机""", "ideal_answer": []},
+# ]
+
+
+# def eval_response_with_ideal(response, ideal, debug=False):
+#     """
+#     评估回复是否与理想答案匹配
+#     参数：
+#     response: 回复的内容
+#     ideal: 理想的答案
+#     debug: 是否打印调试信息
+#     """
+#     if debug:
+#         print("回复：")
+#         print(response)
+#     # json.loads() 只能解析双引号，因此此处将单引号替换为双引号
+#     json_like_str = response.replace("'", '"')
+#     # 解析为一系列的字典
+#     l_of_d = json.loads(json_like_str)
+#     # 当响应为空，即没有找到任何商品时
+#     if l_of_d == [] and ideal == []:
+#         return 1
+#     # 另外一种异常情况是，标准答案数量与回复答案数量不匹配
+#     elif l_of_d == [] or ideal == []:
+#         return 0
+#     # 统计正确答案数量
+#     correct = 0
+#     if debug:
+#         print("l_of_d is")
+#         print(l_of_d)
+#     # 对每一个问答对
+#     for d in l_of_d:
+#         # 获取产品和目录
+#         cat = d.get("category")
+#         prod_l = d.get("products")
+#         # 有获取到产品和目录
+#         if cat and prod_l:
+#             # convert list to set for comparison
+#             prod_set = set(prod_l)
+#         # get ideal set of products
+#         ideal_cat = ideal.get(cat)
+#         if ideal_cat:
+#             prod_set_ideal = set(ideal.get(cat))
+#         else:
+#             if debug:
+#                 print(f"没有在标准答案中找到目录 {cat}")
+#                 print(f"标准答案: {ideal}")
+#             continue
+#         if debug:
+#             print("产品集合：\n", prod_set)
+#             print()
+#             print("标准答案的产品集合：\n", prod_set_ideal)
+#         # 查找到的产品集合和标准的产品集合一致
+#         if prod_set == prod_set_ideal:
+#             if debug:
+#                 print("正确")
+#             correct += 1
+#         else:
+#             print("错误")
+#             print(f"产品集合: {prod_set}")
+#             print(f"标准的产品集合: {prod_set_ideal}")
+#         if prod_set <= prod_set_ideal:
+#             print("回答是标准答案的一个子集")
+#         elif prod_set >= prod_set_ideal:
+#             print("回答是标准答案的一个超集")
+#     # 计算正确答案数
+#     pc_correct = correct / len(l_of_d)
+#     return pc_correct
+
+
+# # print(f'用户提问: {msg_ideal_pairs_set[7]["customer_msg"]}')
+# # print(f'标准答案: {msg_ideal_pairs_set[7]["ideal_answer"]}')
+
+# # response = find_category_and_product_v2(
+# #     msg_ideal_pairs_set[7]["customer_msg"], products_and_category
+# # )
+# # print(f"回答: {response}")
+# # v = eval_response_with_ideal(response, msg_ideal_pairs_set[7]["ideal_answer"])
+# # print(v)
+
+# score_accum = 0
+# for i, pair in enumerate(msg_ideal_pairs_set):
+#     time.sleep(20)
+#     print(f"示例 {i}")
+#     customer_msg = pair['customer_msg']
+#     ideal = pair['ideal_answer']
+#     # print("Customer message",customer_msg)
+#     # print("ideal:",ideal)
+#     response = find_category_and_product_v2(customer_msg,
+#     products_and_category)
+#     # print("products_by_category",products_by_category)
+#     score = eval_response_with_ideal(response,ideal,debug=False)
+#     print(f"{i}: {score}")
+#     score_accum += score
+# n_examples = len(msg_ideal_pairs_set)
+# fraction_correct = score_accum / n_examples
+# print(f"正确比例为 {n_examples}: {fraction_correct}")
+
+# 第十章 评估（下）——不存在简单的正确答案
+# import utils_zh
+# from tool import get_completion_from_messages
+
+# """
+# 注意：限于模型对中文理解能力较弱，中文 Prompt 可能会随机出现不成功，可以多次运行；也非常欢迎同学
+# 探究更稳定的中文 Prompt
+# """
+
+# # 用户消息
+# customer_msg = f"""
+# 告诉我有关 the smartx pro phone 和 the fotosnap camera, the dslr one 的信息。
+# 另外，你们这有什么 TVs ？"""
+
+# # 从问题中抽取商品名
+# products_by_category = utils_zh.get_products_from_query(customer_msg)
+# # 将商品名转化为列表
+# category_and_product_list = utils_zh.read_string_to_list(products_by_category)
+# # 查找商品对应的信息
+# product_info = utils_zh.get_mentioned_product_info(category_and_product_list)
+# # 由信息生成回答
+# # assistant_answer = utils_zh.answer_user_msg(
+# #     user_msg=customer_msg, product_info=product_info
+# # )
+# # print(assistant_answer)
+
+
+# # 问题、上下文
+# # cust_prod_info = {"customer_msg": customer_msg, "context": product_info}
+
+
+# def eval_with_rubric(test_set, assistant_answer):
+#     """
+#     使用 GPT API 评估生成的回答
+#     参数：
+#     test_set: 测试集
+#     assistant_answer: 助手的回复
+#     """
+#     cust_msg = test_set["customer_msg"]
+#     context = test_set["context"]
+#     completion = assistant_answer
+#     # 人设
+#     system_message = """\
+#     你是一位助理，通过查看客户服务代理使用的上下文来评估客户服务代理回答用户问题的情况。
+#     """
+#     # 具体指令
+#     user_message = f"""\
+#     你正在根据代理使用的上下文评估对问题的提交答案。以下是数据：
+#     [开始]
+#     ************
+#     [用户问题]: {cust_msg}
+#     ************
+#     [使用的上下文]: {context}
+#     ************
+#     [客户代理的回答]: {completion}
+#     ************
+#     [结束]
+#     请将提交的答案的事实内容与上下文进行比较，忽略样式、语法或标点符号上的差异。
+#     回答以下问题：
+#     助手的回应是否只基于所提供的上下文？（是或否）
+#     回答中是否包含上下文中未提供的信息？（是或否）
+#     回应与上下文之间是否存在任何不一致之处？（是或否）
+#     计算用户提出了多少个问题。（输出一个数字）
+#     对于用户提出的每个问题，是否有相应的回答？
+#     问题1：（是或否）
+#     问题2：（是或否）
+#     三、评估生成回答与标准回答的差距
+#     在经典的自然语言处理技术中，有一些传统的度量标准用于衡量 LLM 输出与人类专家编写的输出的相似
+#     度。例如，BLUE 分数可用于衡量两段文本的相似程度。
+#     实际上有一种更好的方法，即使用 Prompt。您可以指定 Prompt，使用 Prompt 来比较由 LLM 自动生
+#     成的客户服务代理响应与人工理想响应的匹配程度。
+#     ...
+#     问题N：（是或否）
+#     在提出的问题数量中，有多少个问题在回答中得到了回应？（输出一个数字）
+#     """
+#     messages = [
+#         {"role": "system", "content": system_message},
+#         {"role": "user", "content": user_message},
+#     ]
+#     response = get_completion_from_messages(messages)
+#     return response
+
+
+# # evaluation_output = eval_with_rubric(cust_prod_info, assistant_answer)
+# # print(evaluation_output)
+
+# """基于中文Prompt的验证集"""
+# test_set_ideal = {
+#     "customer_msg": """\
+# 告诉我有关 the Smartx Pro 手机 和 FotoSnap DSLR相机, the dslr one 的信息。\n另外，你们这
+# 有什么电视 ？""",
+#     "ideal_answer": """\
+# SmartX Pro手机是一款功能强大的智能手机，拥有6.1英寸显示屏、128GB存储空间、12MP双摄像头和5G网
+# 络支持。价格为899.99美元，保修期为1年。
+# FotoSnap DSLR相机是一款多功能的单反相机，拥有24.2MP传感器、1080p视频拍摄、3英寸液晶屏和可更
+# 换镜头。价格为599.99美元，保修期为1年。
+# 我们有以下电视可供选择：
+# 1. CineView 4K电视（型号：CV-4K55）- 55英寸显示屏，4K分辨率，支持HDR和智能电视功能。价格为
+# 599.99美元，保修期为2年。
+# 2. CineView 8K电视（型号：CV-8K65）- 65英寸显示屏，8K分辨率，支持HDR和智能电视功能。价格为
+# 2999.99美元，保修期为2年。
+# 3. CineView OLED电视（型号：CV-OLED55）- 55英寸OLED显示屏，4K分辨率，支持HDR和智能电视功
+# 能。价格为1499.99美元，保修期为2年。
+# """,
+# }
+
+
+# def eval_vs_ideal(test_set, assistant_answer):
+#     """
+#     评估回复是否与理想答案匹配
+#     参数：
+#     test_set: 测试集
+#     assistant_answer: 助手的回复
+#     """
+#     cust_msg = test_set["customer_msg"]
+#     ideal = test_set["ideal_answer"]
+#     completion = assistant_answer
+#     system_message = """\
+#     您是一位助理，通过将客户服务代理的回答与理想（专家）回答进行比较，评估客户服务代理对用户问题
+#     的回答质量。
+#     请输出一个单独的字母（A 、B、C、D、E），不要包含其他内容。
+#     """
+#     user_message = f"""\
+#     您正在比较一个给定问题的提交答案和专家答案。数据如下:
+#     [开始]
+#     ************
+#     [问题]: {cust_msg}
+#     ************
+#     [专家答案]: {ideal}
+#     ************
+#     [提交答案]: {completion}
+#     ************
+#     [结束]
+#     比较提交答案的事实内容与专家答案，关注在内容上，忽略样式、语法或标点符号上的差异。
+#     你的关注核心应该是答案的内容是否正确，内容的细微差异是可以接受的。
+#     提交的答案可能是专家答案的子集、超集，或者与之冲突。确定适用的情况，并通过选择以下选项之一回
+#     答问题：
+#     （A）提交的答案是专家答案的子集，并且与之完全一致。
+#     （B）提交的答案是专家答案的超集，并且与之完全一致。
+#     （C）提交的答案包含与专家答案完全相同的细节。
+#     （D）提交的答案与专家答案存在分歧。
+#     （E）答案存在差异，但从事实的角度来看这些差异并不重要。
+#     选项：ABCDE
+#     """
+#     messages = [
+#         {"role": "system", "content": system_message},
+#         {"role": "user", "content": user_message},
+#     ]
+#     response = get_completion_from_messages(messages)
+#     return response
+
+
+# # res = eval_vs_ideal(test_set_ideal, assistant_answer)
+# # print(res)
+
+
+# assistant_answer_2 = "life is like a box of chocolates"
+# res2 = eval_vs_ideal(test_set_ideal, assistant_answer_2)
+# print(res2)
